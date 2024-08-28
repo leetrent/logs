dc0ceadf40ecb660edc470a6b952f4dc119c7bfb
LeeTrent <lee.trent.1@gmail.com>
2024-08-28 07:44:21 -0500

    Added divide function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 use std::fs;
+use std::io::Error;
 
 fn main() {
     let text = fs::read_to_string("logs.txt");
     println!("{:#?}", text);
+}
+
+fn divide(a: f64, b: f64) -> Result<f64, Error> {
+    if b == 0.0 {
+        Err(Error::other("Cannot divide by zero."))
+    } else {
+        Ok (a / b)
+    }
 }
