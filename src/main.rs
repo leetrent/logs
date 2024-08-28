df901174cc49f4d697819ac1e49ce582d3007adf
LeeTrent <lee.trent.1@gmail.com>
2024-08-28 07:54:49 -0500

    Using divide funtion

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,26 @@ use std::fs;
 use std::io::Error;
 
 fn main() {
-    let text = fs::read_to_string("logs.txt");
-    println!("{:#?}", text);
+    // let text = fs::read_to_string("logs.txt");
+    // println!("{:#?}", text);
+    
+    match divide(5.0, 3.0) {
+        Ok(result_of_division) => {
+            println!("\n\nResult of Division: {}", result_of_division);
+        }
+        Err(what_went_wrong) => {
+            println!("What went wrong: {}", what_went_wrong);
+        }
+    }
+
+    match divide(5.0, 0.0) {
+        Ok(result_of_division) => {
+            println!("\n\nResult of Division: {}", result_of_division);
+        }
+        Err(what_went_wrong) => {
+            println!("What went wrong: {}", what_went_wrong);
+        }
+    }
 }
 
 fn divide(a: f64, b: f64) -> Result<f64, Error> {
