b0c0a294c96b15db40079e0ea46c7c68eb2c0eeb
LeeTrent <lee.trent.1@gmail.com>
2024-08-29 08:18:42 -0500

    Just getting started

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 use std::fs;
 
+fn string_test (a: String, b: &String, c: &str) {
+
+}
+
+
 fn main() {
+
+    string_test (
+        String::from("red"),
+        &String::from("red"),
+        "red"
+    );
+
+    string_test (
+        "red".to_string(),
+        &String::from("red"),
+        String::from("red").as_str()
+    );
+
     match fs::read_to_string("logs.txt") {
         Ok(read_text) => {
             println!("# of characters read: {}", read_text.len());
