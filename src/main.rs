fe5b4a969027b07bf5920c18f157e3ab203a95c0
LeeTrent <lee.trent.1@gmail.com>
2024-08-29 07:56:22 -0500

    Now using Result enum when reading file

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,21 @@
 use std::fs;
-use std::io::Error;
 
 fn main() {
-    // let text = fs::read_to_string("logs.txt");
-    // println!("{:#?}", text);
-    
-    match divide(5.0, 3.0) {
-        Ok(result_of_division) => {
-            println!("\n\nResult of Division: {}", result_of_division);
-        }
-        Err(what_went_wrong) => {
-            println!("What went wrong: {}", what_went_wrong);
+    match fs::read_to_string("logs.txt") {
+        Ok(read_text) => {
+            println!("# of characters read: {}", read_text.len());
+        },
+        Err(err_msg) => {
+            println!("{}", err_msg);
         }
     }
 
-    match divide(5.0, 0.0) {
-        Ok(result_of_division) => {
-            println!("\n\nResult of Division: {}", result_of_division);
-        }
-        Err(what_went_wrong) => {
-            println!("What went wrong: {}", what_went_wrong);
-        }
-    }
-
-    match validate_email(String::from("casey@casey.com")) {
-        Ok(..) => println!("\nEmail address is valid"),
-        Err(error_message) => {
-            println!("\nValidation Error Message: {}", error_message)
+    match fs::read_to_string("no_file.txt") {
+        Ok(read_text) => {
+            println!("# of characters read: {}", read_text.len());
+        },
+        Err(err_msg) => {
+            println!("{}", err_msg);
         }
     }
-
-    match validate_email(String::from("caseycasey.com")) {
-        Ok(..) => println!("\nEmail address is valid"),
-        Err(error_message) => {
-            println!("\nValidation Error Message: {}", error_message)
-        }
-    }
-}
-
-fn divide(a: f64, b: f64) -> Result<f64, Error> {
-    if b == 0.0 {
-        Err(Error::other("Cannot divide by zero."))
-    } else {
-        Ok (a / b)
-    }
-}
-
-fn validate_email(email: String) -> Result<(), Error> {
-    if email.contains("@") {
-        Ok(())
-    } else {
-        Err(Error::other("Email addresses must have an ampersand."))
-    }
-
 }
