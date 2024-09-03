2b2eb6bee0b82d1082121422975391853a2a67c4
LeeTrent <lee.trent.1@gmail.com>
2024-09-03 08:13:27 -0500

    Successfully reading error logs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
 use std::fs;
 
+fn extract_errors(text: &str) -> Vec<&str> {
+    let split_text = text.split("\n");
+    let mut results = vec![];
+
+    for line in split_text {
+        if line.starts_with("ERROR") {
+            results.push(line);
+        }
+    }
+    results
+}
+
 fn main() {
     match fs::read_to_string("logs.txt") {
         Ok(read_text) => {
-            println!("# of characters read: {}", read_text.len());
+            let error_logs = extract_errors(read_text.as_str());
+            println!("{:#?}", error_logs);
         },
         Err(err_msg) => {
             println!("{}", err_msg);
